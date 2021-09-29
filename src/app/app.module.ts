166c0295ac64d9e6630549360d4f20ddf192dc4a
cybrisha <cybrisha@gmail.com>
2021-09-29 16:18:27 +0300

    add bootstrap icons, layout clean-up

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,19 @@ import { AppComponent } from './app.component';
 import { FirstComponent } from './first/first.component';
 import { ProductsModule } from './products/products.module';
 import { CartModule } from './cart/cart.module';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FirstComponent
+    FirstComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     ProductsModule,
-    CartModule
+    CartModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
