c81c1bd26e41ea53116bf2d6086a2a0778e45f41
cybrisha <cybrisha@gmail.com>
2021-10-07 17:27:13 +0300

    feat: @ViewChild, app title

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements OnInit, AfterViewInit {
+  @ViewChild('appTitle', { static: false })
+  title!: ElementRef<HTMLHeadingElement>;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngAfterViewInit(): void {
+    this.title.nativeElement.textContent = 'TopShop';
   }
 
+  ngOnInit(): void {}
 }
